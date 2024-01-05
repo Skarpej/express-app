697892adee2aa1faed145d78c34b2d038ca257db
“Johannes <johannes.karpe@gmail.com>
2024-01-05 14:28:49 +0100

    feat: added saving name

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ const port = 3000
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/greeting', (req, res) => {
-  res.send('Hi!')
+    console.log(theName)
+    res.send('Hi!')
 })
 
+let theName = ""
+
 app.post('/greeting', (req, res) => {
-  const { name } = req.body;
-  res.sendStatus(200);
+    const { name } = req.body;
+    theName = name;
+    res.sendStatus(200);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
